3bfc2005fd8dea5cd1df2b3a7f40303d11abf288
Faelin Landy <faelin.landy@gmail.com>
2017-12-24 00:01:09 -0500

    added SummarizeAll.pl script

--- scripts/SummarizeAll.pl
+++ scripts/SummarizeAll.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/env perl
 use Text::Summarizer;
 
-my $summarizer = Text::Summarizer->new;
+my $summarizer = Text::Summarizer->new(
+		articles_path => 'articles/*',
+		permanent_path => 'data/permanent.stop',
+		stopwords_path => 'data/stopwords.stop',
+		watchlist_path => 'data/watchlist.stop',
+	);
 
 $summarizer->scan_all;
 $summarizer->summarize_all;
