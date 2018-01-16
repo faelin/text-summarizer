f3ff7e46f133b401f52009180b4239e036a360c1
Faelin Landy <faelin.landy@gmail.com>
2018-01-16 15:54:05 -0500

    SummarizeAll script currently does not output to file

--- scripts/SummarizeAll.pl
+++ scripts/SummarizeAll.pl
@@ -7,8 +7,8 @@ my $summarizer = Text::Summarizer->new;
 # $summarizer->scan_all;
 $summarizer->summarize_all;
 
-open( my $file, '>>', sprintf( "data/phrases.csv", $i) )
-		or die "Can't open data/phrases.csv: $!";
+#open( my $file, '>>', sprintf( "data/phrases.csv", $i) )
+#		or die "Can't open data/phrases.csv: $!";
 
 my $phrases   = $summarizer->phrase_list;
 my @phrase_list = sort { $phrases->{$b} <=> $phrases->{$a} } keys %$phrases;
@@ -16,7 +16,7 @@ my $highest = $phrase_list[0];
 for my $sen ( sort { $phrases->{$b} <=> $phrases->{$a} } keys %$phrases ) {
 	my $phrase = join ' ' => split /[^A-Za-z0-9-']+/ => $sen;
 	my $score = $phrases->{$sen} / $phrases->{$highest};
-	#print $file "$phrase,$score\n";
+#	print $file "$phrase,$score\n";
 }
 
-close $file;
+#close $file;
