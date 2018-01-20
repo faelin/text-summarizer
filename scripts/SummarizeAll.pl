2e5b85f3271241fe69431f8dd7a328ac6e540c53
Faelin Landy <faelin.landy@gmail.com>
2018-01-19 19:49:59 -0500

    commented out phrase-output block for testing

--- scripts/SummarizeAll.pl
+++ scripts/SummarizeAll.pl
@@ -30,16 +30,19 @@ my $text_summ = $summarizer->summarize_text(<<'END_SAMPLE');
 END_SAMPLE
 
 
-open( my $file, '>>', sprintf( "data/phrases.csv", $i) )
-		or die "Can't open data/phrases.csv: $!";
+$summarizer->pretty_print($file_summ, 20);
 
-my $phrases = $summarizer->phrase_list;
-my @phrase_list = sort { $phrases->{$b} <=> $phrases->{$a} } keys %$phrases;
-my $highest = $phrase_list[0];
-for my $sen ( sort { $phrases->{$b} <=> $phrases->{$a} } keys %$phrases ) {
-	my $phrase = join ' ' => split /[^A-Za-z0-9-']+/ => $sen;
-	my $score = $phrases->{$sen} / $phrases->{$highest};
-	# print $file "$phrase,$score\n";
-}
 
-close $file;
+# open( my $file, '>>', sprintf( "data/phrases.csv", $i) )
+# 		or die "Can't open data/phrases.csv: $!";
+		
+# my $phrases = $summarizer->phrase_list;
+# my @phrase_list = sort { $phrases->{$b} <=> $phrases->{$a} } keys %$phrases;
+# my $highest = $phrase_list[0];
+# for my $sen ( sort { $phrases->{$b} <=> $phrases->{$a} } keys %$phrases ) {
+# 	my $phrase = join ' ' => split /[^A-Za-z0-9-']+/ => $sen;
+# 	my $score = $phrases->{$sen} / $phrases->{$highest};
+# 	# print $file "$phrase,$score\n";
+# }
+
+# close $file;
